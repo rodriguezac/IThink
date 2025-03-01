56c082fcd474b6d9ae6f74780654a7f528f990c7
Angélica Rodríguez <rodriguezq.angelicac@gmail.com>
2025-03-01 00:55:52 -0500

    Ajustes responsive
    Cambio de tamaño de letra

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,11 +3,13 @@ import React from "react";
 const Hero = () => {
     return (
         <section className="text-center py-16 bg-[#F0E7D8]">
-            <h1 className="text-5xl font-bold text-[#6485AE]">¿Tienes una idea en mente?</h1>
+            <h1 className="text-4xl sm:text-5xl font-bold text-[#6485AE]">
+                ¿Tienes una idea en mente?
+            </h1>
             <button className="mt-6 bg-[#F7D881] text-[#292B3A] font-semibold px-6 py-2 border-3 border-[#D2B45E] hover:scale-105 transition-transform">
                 Escríbela Aquí
             </button>
-            <p className="mt-4 text-gray-600 text-xl">
+            <p className="mt-4 text-gray-600 text-xl sm:text-lg">
                 siente,   escribe,   comparte.
             </p>
         </section>
